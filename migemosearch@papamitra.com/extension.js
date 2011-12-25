d5834c0cdb7614cdcaa5b692a814534f5c47f5fd
Taiki Sugawara <buzz.taiki@gmail.com>
2011-12-26 05:23:46 +0900

    Used cmigemo instead of migemo-client.

--- migemosearch@papamitra.com/extension.js
+++ migemosearch@papamitra.com/extension.js
@@ -1,4 +1,4 @@
-// Sample extension code, makes clicking on the panel show a message
+//-*- mode:js; js-indent-level: 4-*-
 const St = imports.gi.St;
 const Mainloop = imports.mainloop;
 
@@ -6,6 +6,11 @@ const Main = imports.ui.main;
 const Search = imports.ui.search;
 const AppDisplay = imports.ui.appDisplay;
 const GLib = imports.gi.GLib;
+const Gio = imports.gi.Gio;
+
+const MIGEMO = '/usr/bin/cmigemo';
+const MIGEMO_DICT = '/usr/share/cmigemo/utf-8/migemo-dict';
+const MIGEMO_MIN_TERMS = 2;
 
 // Put your extension initialization code here
 function main() {
@@ -14,14 +19,14 @@ function main() {
 
     Main.overview.viewSelector.addSearchProvider(migemo);
 
-   Search.SearchSystem.prototype.updateSearch_orig = Search.SearchSystem.prototype.updateSearch;
+    Search.SearchSystem.prototype.updateSearch_orig = Search.SearchSystem.prototype.updateSearch;
     Search.SearchSystem.prototype.updateSearch = function(searchString){
-	let results = this.updateSearch_orig(searchString);
-	let res = migemo.getResultSet(searchString);
-	if(res.length > 0){
-	    results.push([migemo, res]);
-	}
-	return results;
+        let results = this.updateSearch_orig(searchString);
+        let res = migemo.getResultSet(searchString);
+        if(res.length > 0){
+            results.push([migemo, res]);
+        }
+        return results;
     }
 }
 
@@ -34,36 +39,68 @@ MigemoSearchProvider.prototype = {
 
     _init: function() {
         AppDisplay.BaseAppSearchProvider.prototype._init.call(this, "migemo");
+        this._migemo = new Migemo(MIGEMO, MIGEMO_DICT);
     },
 
     getInitialResultSet: function(terms) {
-	// dummy
-	return [];
+        // dummy
+        return [];
     },
 
     getSubsearchResultSet: function(previousResults, terms) {
-	// dummy
-	return [];
+        // dummy
+        return [];
     },
 
     getResultSet: function(terms) {
-	if(terms.length < 3) { return []; }
+        if (terms.length < MIGEMO_MIN_TERMS) { return []; }
 
-	let migemo_ret = GLib.spawn_command_line_sync('/bin/sh -c "migemo-client ' + terms + ' |nkf -w8"');
-	global.logError(migemo_ret);
-	if(!migemo_ret[0]){ return []; }
+        let searchString = this._migemo.query(terms);
+        global.log(searchString);
 
-	let searchString = migemo_ret[1].replace(/(\n|\r)+$/, '');
-	let regexp = new RegExp(searchString);
-	let apps = this._appSys.get_flattened_apps(); // get all apps
-	return apps.filter(function(app){
-			       return -1 < app.get_name().search(regexp);
-			   }).map(function(app){ return app.get_id();});
+        let regexp = new RegExp(searchString);
+        let apps = this._appSys.get_flattened_apps(); // get all apps
+        return apps.filter(function(app){
+            return -1 < app.get_name().search(regexp);
+        }).map(function(app){
+            return app.get_id();
+        });
     },
 
     createResultActor: function (resultMeta, terms) {
         let app = this._appSys.get_app(resultMeta['id']);
         let icon = new AppDisplay.AppWellIcon(app);
         return icon.actor;
+    }
+};
+
+function Migemo(migemo, migemoDict) {
+    this._init(migemo, migemoDict);
+}
+
+Migemo.prototype = {
+    _init: function(migemo, migemoDict) {
+        let [res, pid, stdinFd, stdoutFd, stderrFd]  = GLib.spawn_async_with_pipes(
+            null,
+            [migemo, '-d', migemoDict, '-q', '-n'],
+            null, GLib.SpawnFlags.SEARCH_PATH, null);
+        if (!res) {
+            throw 'Failed to spwan ' + migemo;
+        }
+        this._pid = pid;
+        this._stdin = new Gio.DataOutputStream({
+            base_stream: new Gio.UnixOutputStream({fd: stdinFd})
+        });
+        this._stdout = new Gio.DataInputStream({
+            base_stream: new Gio.UnixInputStream({fd: stdoutFd})
+        });
+        global.log('migemo initialized');
+    },
+
+    query: function(query) {
+        this._stdin.put_string(query + '\n', null);
+        // XXX: Want to use cancellable.
+        let [out, size] = this._stdout.read_line(null);
+        return out;
     }
 };
