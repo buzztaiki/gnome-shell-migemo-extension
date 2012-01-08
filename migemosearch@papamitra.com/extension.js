4f6b69bb279dc1b6b5a055281e6805ebf4e7591e
Taiki Sugawara <buzz.taiki@gmail.com>
2012-01-09 03:27:25 +0900

    Support gnome-shell 3.2. But not yet support gnome-shell 3.0.

--- migemosearch@papamitra.com/extension.js
+++ migemosearch@papamitra.com/extension.js
@@ -1,80 +1,81 @@
 //-*- mode:js; js-indent-level: 4-*-
-const St = imports.gi.St;
-const Mainloop = imports.mainloop;
-
 const Main = imports.ui.main;
 const Search = imports.ui.search;
 const AppDisplay = imports.ui.appDisplay;
 const GLib = imports.gi.GLib;
 const Gio = imports.gi.Gio;
+const Shell = imports.gi.Shell;
 
 const MIGEMO = '/usr/bin/cmigemo';
 const MIGEMO_DICT = '/usr/share/cmigemo/utf-8/migemo-dict';
-const MIGEMO_MIN_TERMS = 2;
+const MIGEMO_MIN_LENGTH = 2;
 
-function main() {
-    let migemoProvider = new MigemoSearchProvider();
-
-    Main.overview.viewSelector.addSearchProvider(migemoProvider);
-
-    Search.SearchSystem.prototype.updateSearch_orig = Search.SearchSystem.prototype.updateSearch;
-    Search.SearchSystem.prototype.updateSearch = function(searchString) {
-        let results = this.updateSearch_orig(searchString);
-        let resultIds = results.reduce(function(acc, [provider, providerResults]) {
-            return acc.concat(providerResults);
-        }, []);
-        let res = migemoProvider.getResultSet(searchString).filter(function(id) {
-            return resultIds.indexOf(id) < 0;
-        });
-        if(res.length > 0) {
-            results.push([migemoProvider, res]);
-        }
-        return results;
-    }
-}
-
-function MigemoSearchProvider() {
-    this._init();
+function MigemoSearchProvider(migemo) {
+    this._init(migemo);
 }
 
 MigemoSearchProvider.prototype = {
-    __proto__: AppDisplay.BaseAppSearchProvider.prototype,
+    __proto__: Search.SearchProvider.prototype,
 
-    _init: function() {
-        AppDisplay.BaseAppSearchProvider.prototype._init.call(this, "migemo");
-        this._migemo = new Migemo(MIGEMO, MIGEMO_DICT);
+    _init: function(migemo) {
+        Search.SearchProvider.prototype._init.call(this, "migemo");
+        this._migemo = migemo;
+        this._appSys = Shell.AppSystem.get_default();
     },
 
     getInitialResultSet: function(terms) {
-        // dummy
-        return [];
+        let results = this._getResultSet(terms.join(""));
+        if (results.length == 0) {
+            return results;
+        }
+        return this._reduceResults(results, this._appSys.initial_search(terms));
     },
 
     getSubsearchResultSet: function(previousResults, terms) {
-        // dummy
-        return [];
+        let results = this._getResultSet(terms.join(""));
+        if (results.length == 0) {
+            return results;
+        }
+        return this._reduceResults(results, this._appSys.subsearch(previousResults, terms));
     },
 
-    getResultSet: function(terms) {
-        if (terms.length < MIGEMO_MIN_TERMS) {
-            return [];
-        }
-
-        let searchString = this._migemo.query(terms);
-        let regexp = new RegExp(searchString);
-        let apps = this._appSys.get_flattened_apps(); // get all apps
-        return apps.filter(function(app) {
-            return -1 < app.get_name().search(regexp);
-        }).map(function(app) {
-            return app.get_id();
+    _reduceResults: function(results, appResults) {
+        let appNames = appResults.map(function(app) {
+            return app.get_name();
+        });
+        return results.filter(function(app) {
+            return appNames.indexOf(app.get_name()) < 0;
         });
     },
 
+    _getResultSet: function(searchString) {
+        if (searchString.length < MIGEMO_MIN_LENGTH) {
+            return [];
+        }
+
+        let queryResult = this._migemo.query(searchString);
+        let regexp = new RegExp(queryResult);
+        let apps = this._appSys.get_all();
+        return apps.filter(function(app) {
+            return -1 < app.get_name().search(regexp);
+        });
+    },
+
+    getResultMeta: function(app) {
+        return AppDisplay.AppSearchProvider.prototype.getResultMeta.call(this, app);
+    },
+
+    activateResult: function(app, params) {
+        return AppDisplay.AppSearchProvider.prototype.activateResult.call(this, app, params);
+    },
+
+    dragActivateResult: function(id, params) {
+        return AppDisplay.AppSearchProvider.prototype.dragActivateResult.call(this, id, params);
+    },
+
     createResultActor: function (resultMeta, terms) {
-        let app = this._appSys.get_app(resultMeta['id']);
-        let icon = new AppDisplay.AppWellIcon(app);
-        return icon.actor;
-    }
+        return AppDisplay.AppSearchProvider.prototype.createResultActor.call(this, resultMeta, terms);
+    },
 };
 
 function Migemo(migemo, migemoDict) {
@@ -109,5 +110,40 @@ Migemo.prototype = {
         // XXX: Want to use cancellable.
         let [out, size] = this._stdout.read_line(null);
         return out;
-    }
+    },
+
+    dispose: function() {
+        GLib.spawn_close_pid(this._pid);
+    },
 };
+
+function MigemoSearchExtension() {
+    this._init();
+}
+
+MigemoSearchExtension.prototype = {
+    _init: function() {
+        // do nothing.
+    },
+
+    enable: function() {
+        this._migemo = new Migemo(MIGEMO, MIGEMO_DICT);
+        this._migemoProvider = new MigemoSearchProvider(this._migemo);
+
+        Main.overview.addSearchProvider(this._migemoProvider);
+    },
+
+    disable: function() {
+        Main.overview.removeSearchProvider(this._migemoProvider);
+        this._migemo.dispose();
+        this._migemo = null;
+        this._migemoProvider = null;
+    },
+};
+
+function init() {
+    return new MigemoSearchExtension();
+}
+
+function main() {
+}
